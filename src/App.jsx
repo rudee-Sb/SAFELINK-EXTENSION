57006eb287cf71cafca07a8aea2c965f7a2c48e4
Rudee-Sb <rudrabhau844@gmail.com>
2025-11-03 20:32:42 +0530

    files created, tailwind setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <div className="text-3xl font-bold text-green-400">Safelink is live ✅</div>
+
+        </>
+    )
+}
+
+export default App
